762b90d148c746702200307d9b5c479df3612af1
Abdullah-AlQahtani <abdullahq.ksa@gmail.com>
2019-01-09 17:37:43 +0300

    first commit

---
+++ src/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { View, Text, ImageBackground, Dimensions } from 'react-native';
+import { Button } from 'react-native-elements'
+import styles from './HomeScreenStyle';
+import colors from '../../Config';
+// import { createStackNavigator } from 'react-navigation';
+
+export default class HomeScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        const { width, height } = Dimensions.get('window');
+        return (
+            <View style={styles.container}>
+                <ImageBackground
+                    style={{ width: '100%', height: '100%' }}
+                    source={require('../../images/backgroud.jpg')} >
+                    <View style={styles.container}>
+                        <View style={{ padding: 10, }}><Button
+                            buttonStyle={{ borderColor: colors.secondColor, borderWidth: 3, borderRadius: 10, }}
+                            containerViewStyle={{ width: width / 1.2 }}
+                            backgroundColor={colors.baseColor}
+                            large title="Show Food Lists" />
+                        </View>
+                        <View><Button
+                            buttonStyle={{ borderColor: colors.secondColor, borderWidth: 3, borderRadius: 10, }}
+                            backgroundColor={colors.baseColor}
+                            containerViewStyle={{ width: width / 1.2 }}
+                            large title="Add New Food" /></View>
+                    </View>
+                </ImageBackground>
+            </View>
+        );
+    }
+}
